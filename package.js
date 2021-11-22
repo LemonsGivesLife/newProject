ae453886eeab56ddd70401354d4be59223fb3998
LemonsGivesLife <33992350+LemonsGivesLife@users.noreply.github.com>
2021-11-22 12:25:35 +1100

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "customerOrderSystem",
+  "version": "1.0.0",
+  "description": "node app ",
+  "main": "myScript.js",
+  "scripts": {
+  "start": "node myScript.js"
+},
+  "repository": {
+  "type": "git",
+  "url": ""
+},
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+  "url": ""
+},
+  "homepage": ""
+}
